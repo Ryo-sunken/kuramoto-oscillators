919b5917fbe7e6f9043eb8eaac1b420ade918f48
Ryo-sunken <bellman.toratora@gmail.com>
2024-10-06 19:21:55 +0900

    パラメータのチェック

--- src/parameter.rs
+++ src/parameter.rs
@@ -20,7 +20,6 @@ impl CommonParam {
 pub struct NetworkParam {
     comment: String,
     pub state_dim: usize,
-    pub input_dim: usize,
     pub cluster_nodes_num: Vec<usize>,
     pub frequency: Vec<f64>,
     pub connectivity: Vec<Vec<f64>>,
@@ -34,9 +33,24 @@ impl NetworkParam {
     }
 
     fn check(&self) {
-        // freqとconnectivityのshape
+        // shape
+        assert_eq!(self.cluster_nodes_num.iter().sum::<usize>(), self.state_dim);
+        assert_eq!(self.frequency.len(), self.state_dim);
+        assert_eq!(self.connectivity.len(), self.state_dim);
+        for i in 0..self.connectivity.len() {
+            assert_eq!(self.connectivity[i].len(), self.state_dim);
+        }
 
         // connectivityが非負で対称
+        assert!(self.connectivity.iter().flatten().all(|&x| x >= 0.));
+        for i in 0..self.state_dim {
+            for j in i..self.state_dim {
+                if self.connectivity[i][j] != self.connectivity[j][i] {
+                    panic!("connectivity is not symmetric.");
+                }
+            }
+        }
+
     }
 }
 
@@ -44,6 +58,8 @@ impl NetworkParam {
 pub struct ControlParam {
     comment: String,
     pub control_type: usize,
+    pub input_dim: usize,
+    pub cluster_nodes_num: Vec<usize>,
     pub input_weight: Vec<f64>,
     pub input_frequency: Vec<f64>,
     pub average_weight: Vec<Vec<f64>>,
@@ -57,8 +73,14 @@ impl ControlParam {
     }
 
     fn check(&self) {
-        // freqとconnectivityのshape
+        // shape
+        assert_eq!(self.cluster_nodes_num.iter().sum::<usize>(), self.input_dim);
+        assert_eq!(self.input_weight.len(), self.input_dim);
+        assert_eq!(self.input_frequency.len(), self.input_dim);
+        assert_eq!(self.average_weight.iter().map(|arr| arr.iter().count()).collect::<Vec<_>>(), self.cluster_nodes_num);
 
-        // connectivityが非負で対称
+        // weightが非負
+        assert!(self.input_weight.iter().all(|&x| x >= 0.));
+        assert!(self.average_weight.iter().flatten().all(|&x| x >= 0.));
     }
 }
